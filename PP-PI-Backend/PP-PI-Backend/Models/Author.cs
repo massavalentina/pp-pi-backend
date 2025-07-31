4a04cf692c73a7c182954bf042c393c4bfb23eeb
joaquincarcano <joaquin.carcano@mi.unc.edu.ar>
2025-07-31 17:41:47 -0300

    Update de propiedades de Author
    Migración para que las propiedades BirthDate y Nationality sean nullables

--- PP-PI-Backend/PP-PI-Backend/Models/Author.cs
+++ PP-PI-Backend/PP-PI-Backend/Models/Author.cs
@@ -6,9 +6,9 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
-        public string Nationality { get; set; }
+        public string? Nationality { get; set; }
 
-        public DateTime Birthdate { get; set; }
+        public DateTime? Birthdate { get; set; }
         public ICollection<Book> Books { get; set;} = new List<Book>(); // List of books that allows inverted navegation
     }
 }
