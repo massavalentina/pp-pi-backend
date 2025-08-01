0e03f9abffc977a69fac6097e050daa7445e33fa
joaquincarcano <joaquin.carcano@mi.unc.edu.ar>
2025-08-01 15:25:30 -0300

    DTO para creación de autores

---
+++ PP-PI-Backend/PP-PI-Backend/DTO/AuthorCreateDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace PP_PI_Backend.DTO
+{
+    public class AuthorCreateDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? Nationality { get; set; }
+        public DateTime? Birthdate { get; set; }
+    }
+}
