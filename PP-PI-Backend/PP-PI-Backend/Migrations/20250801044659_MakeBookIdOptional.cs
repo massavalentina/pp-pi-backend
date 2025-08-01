b377d5a8cafcd5186e7dde51db191399e1be18f9
Franco Herrera <fhiacc@gmail.com>
2025-08-01 15:35:05 -0300

    Review Correjido

---
+++ PP-PI-Backend/PP-PI-Backend/Migrations/20250801044659_MakeBookIdOptional.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PP_PI_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeBookIdOptional : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Books_BookId",
+                table: "Reviews");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BookId",
+                table: "Reviews",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Books_BookId",
+                table: "Reviews",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Books_BookId",
+                table: "Reviews");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BookId",
+                table: "Reviews",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Books_BookId",
+                table: "Reviews",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
