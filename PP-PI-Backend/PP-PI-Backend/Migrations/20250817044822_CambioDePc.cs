764aee57efa0baca10a97c5f3a834778c2e5bf95
Franco Herrera <fhiacc@gmail.com>
2025-09-19 21:36:19 -0300

    Cmmit 19/9
    asdasf

---
+++ PP-PI-Backend/PP-PI-Backend/Migrations/20250817044822_CambioDePc.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PP_PI_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class CambioDePc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Books_BookId",
+                table: "Reviews");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BookId",
+                table: "Reviews",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Books_BookId",
+                table: "Reviews",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Books_BookId",
+                table: "Reviews");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BookId",
+                table: "Reviews",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Books_BookId",
+                table: "Reviews",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "Id");
+        }
+    }
+}
